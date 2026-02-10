8df9a49c81bd35b820ea54bc6e5522e1bb9491b6
Sithum Fernando <sithumdf@gmail.com>
2026-02-10 19:43:36 +0530

    Notifications - DB Migrations

---
+++ drizzle/0018_ Notifications Table Updated.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "notifications" ADD COLUMN "title" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "notifications" ADD COLUMN "type" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "notifications" ADD COLUMN "is_read" boolean DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE "notifications" ADD COLUMN "link" text;--> statement-breakpoint
+ALTER TABLE "notifications" ADD COLUMN "link_label" text;--> statement-breakpoint
+DROP TYPE "public"."competition_category";
