2b45ba7d3a06e1dbb7fbcf6836fad9ac2f0f6f58
dulranga <dulranga.webdesign@gmail.com>
2026-02-10 20:27:33 +0530

    fixed db migration

--- drizzle/0018_ Notifications Table Updated.sql
+++ drizzle/0018_ Notifications Table Updated.sql
@@ -1,6 +1,11 @@
-ALTER TABLE "notifications" ADD COLUMN "title" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "notifications" ADD COLUMN "type" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "notifications" ADD COLUMN "is_read" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "notifications" ADD COLUMN "link" text;--> statement-breakpoint
-ALTER TABLE "notifications" ADD COLUMN "link_label" text;--> statement-breakpoint
-DROP TYPE "public"."competition_category";
+ALTER TABLE "notifications" ADD COLUMN "title" text NOT NULL;
+--> statement-breakpoint
+ALTER TABLE "notifications" ADD COLUMN "type" text NOT NULL;
+--> statement-breakpoint
+ALTER TABLE "notifications"
+ADD COLUMN "is_read" boolean DEFAULT false NOT NULL;
+--> statement-breakpoint
+ALTER TABLE "notifications" ADD COLUMN "link" text;
+--> statement-breakpoint
+ALTER TABLE "notifications" ADD COLUMN "link_label" text;
+--> statement-breakpoint
