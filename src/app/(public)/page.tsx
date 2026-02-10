e85a845af5b3c4fe7a9741ad0a48c29648b8e94e
Sithum Fernando <sithumdf@gmail.com>
2026-02-10 23:53:47 +0530

    Final UI Changes

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="flex flex-col min-h-screen bg-gradient-to-b from-background via-background to-primary/5">
             <Hero />
             <Services />
-            <Footer />
+            {/* <Footer /> */}
             <FooterBottom />
         </div>
     );
